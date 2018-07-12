ae380fe50dd4a402e82852e6c28ef78f684cdfc2
mpatel-402455 <40845246+mpatel-402455@users.noreply.github.com>
2018-07-11 21:47:31 -0400

    Add

---
+++ Remove-Old_AD_Computer_Object.ps1
@@ -0,0 +1,21 @@
+﻿$ServerNames = import-csv -path "C:\MyScripts\Win2K3\DecomServerList.csv"
+
+foreach ($ServerName in $ServerNames)
+    {
+        "`n"
+        $ServerName = $ServerName.HostName
+        Write-Host $ServerName -ForegroundColor Yellow
+
+        Remove-ADComputer $ServerName -Confirm:$false
+
+        $results = Get-ADComputer $ServerName -ErrorAction SilentlyContinue
+        
+        if ($results -ne $NULL)
+            {
+                Write-Host "Failed to delete: $ServerName" -ForegroundColor Red
+               $ServerName | Out-File -FilePath C:\MyScripts\Win2K3\RemoveFail.txt -Append
+            }
+    }
+
+
+Get-ADComputer SrvDC001
